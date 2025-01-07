a4f12a4afc188b5a96ad892859effb1b72f02084
Cristoph Adam Sugianto <giezbungadeza@gmail.com>
2025-01-08 01:07:02 +0700

    fixing font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Inter } from 'next/font/google';
 import Navbar from '../components/Navbar';
 import { ReactNode } from 'react';
 
@@ -7,9 +8,14 @@ export const metadata = {
 	description: 'Connecting people with calls, chats and more',
 };
 
+const inter = Inter({
+	subsets: ['latin'],
+	fallback: ['Arial', 'sans-serif'],
+  });
+
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={inter.className}>
 			<body>
 				<Navbar />
 				{children}
